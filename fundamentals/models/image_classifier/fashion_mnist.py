64c889c799db5ac59ac2bddd592636b83197830e
jancewicz <jancewicz.pawel1@gmail.com>
2026-01-11 12:34:28 +0100

    Created image classifier for fashion MNIST dataset. Training + model evaluation with acc as metric

---
+++ fundamentals/models/image_classifier/fashion_mnist.py
@@ -0,0 +1,45 @@
+import torch
+import torchmetrics
+import torchvision
+import torchvision.transforms.v2 as T
+from torch.utils.data import DataLoader
+
+from fundamentals.models.regression_mlp.regression_mlp import train, evaluate
+from fundamentals.models.image_classifier.image_classifier import model, xentropy
+
+device = "cpu"
+
+# data processing function
+toTensor = T.Compose([T.ToImage(), T.ToDtype(torch.float32, scale=True)])
+
+# transform fashion MNIST from PIL to PyTorch float tensors with scaled pixel values
+train_and_validate_data = torchvision.datasets.FashionMNIST(
+    root="datasets", train=True, download=True, transform=toTensor
+)
+test_data = torchvision.datasets.FashionMNIST(
+    root="datasets", train=False, download=True, transform=toTensor
+)
+
+torch.manual_seed(42)
+train_data, valid_data = torch.utils.data.random_split(
+    train_and_validate_data, [55_000, 5_000]
+)
+
+# create data loaders
+train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
+valid_loader = DataLoader(valid_data, batch_size=32)
+test_loader = DataLoader(test_data, batch_size=32)
+
+learning_rate = 0.002
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+n_epochs = 20
+
+train(model, optimizer, criterion=xentropy, train_loader=train_loader, n_epochs=n_epochs)
+
+# evaluate model function
+accuracy = torchmetrics.Accuracy(task="multiclass", num_classes=10).to(device)
+evaluation = evaluate(
+    model, valid_loader, accuracy
+)
+
+print(f"Accuracy on validation set: {evaluation}")
---
+++ fundamentals/models/image_classifier/image_classifier.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+
+
+class ImageClassifier(nn.Module):
+    def __init__(self, n_inputs, n_hidden1, n_hidden2, n_classes):
+        super().__init__()
+        self.mlp = nn.Sequential(
+            nn.Flatten(),  # reshape input to single dimension for linear layers
+            nn.Linear(n_inputs, n_hidden1),
+            nn.ReLU(),
+            nn.Linear(n_hidden1, n_hidden2),
+            nn.ReLU(),
+            nn.Linear(n_hidden2, n_classes)
+        )
+
+    def forward(self, X):
+        return self.mlp(X)
+
+
+torch.manual_seed(42)
+model = ImageClassifier(
+    n_inputs=28 * 28,
+    n_hidden1=300,
+    n_hidden2=100,
+    n_classes=10
+)
+
+xentropy = nn.CrossEntropyLoss()
