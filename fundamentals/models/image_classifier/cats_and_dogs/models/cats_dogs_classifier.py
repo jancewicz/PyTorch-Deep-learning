1d07e40808b1724c065de1e712d119dd39090a43
jancewicz <jancewicz.pawel1@gmail.com>
2026-01-17 21:07:33 +0100

    builded custom classifier based on resnet18 architecture

---
+++ fundamentals/models/image_classifier/cats_and_dogs/models/cats_dogs_classifier.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+from torchvision.models.resnet import resnet18, ResNet18_Weights
+
+
+class CatsDogsClassifier(nn.Module):
+    """
+    Cats and dogs classifier class based on resnet18 neural network architecture
+    """
+    def __init__(self, pretrained: bool):
+        super().__init__()
+        if pretrained:
+            weights = ResNet18_Weights.DEFAULT
+            self.resnet = resnet18(weights)
+        else:
+            self.resnet = resnet18(weights=None)
+
+        input_features = self.resnet.fc.in_features
+        self.resnet.fc = nn.Linear(input_features, 2)
+        
+
+
+
+    def forward(self, X: torch.Tensor):
+        return self.resnet(X)
