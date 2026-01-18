165f2dc4c69b2a3bcf4b3d45fe353d56ec204eaa
jancewicz <jancewicz.pawel1@gmail.com>
2026-01-18 11:21:42 +0100

    Created CNN with AlexNet architecture classifier

--- fundamentals/models/image_classifier/cats_and_dogs/predictors/cats_dogs_resnet.py
+++ fundamentals/models/image_classifier/cats_and_dogs/predictors/cats_dogs_resnet.py
@@ -2,11 +2,14 @@ import torch
 import torch.nn as nn
 from torchvision.models.resnet import resnet18, ResNet18_Weights
 
+from utils.device import get_device
 
-class CatsDogsClassifier(nn.Module):
+
+class ResNetCatsDogsClassifier(nn.Module):
     """
     Cats and dogs classifier class based on resnet18 neural network architecture
     """
+
     def __init__(self, pretrained: bool):
         super().__init__()
         if pretrained:
@@ -17,9 +20,12 @@ class CatsDogsClassifier(nn.Module):
 
         input_features = self.resnet.fc.in_features
         self.resnet.fc = nn.Linear(input_features, 2)
-        
-
-
 
     def forward(self, X: torch.Tensor):
         return self.resnet(X)
+
+
+device = get_device()
+
+resnet_model = ResNetCatsDogsClassifier(pretrained=True)
+resnet_model = resnet_model.to(device)
