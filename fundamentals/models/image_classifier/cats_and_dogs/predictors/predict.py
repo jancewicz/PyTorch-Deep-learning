05c7a3a3d47133a3234fcc8b2baf2f99deb23614
jancewicz <jancewicz.pawel1@gmail.com>
2026-01-19 23:23:00 +0100

    black for predict.py

--- fundamentals/models/image_classifier/cats_and_dogs/predictors/predict.py
+++ fundamentals/models/image_classifier/cats_and_dogs/predictors/predict.py
@@ -4,26 +4,28 @@ import torch.nn.functional as F
 from PIL import Image
 from torchvision.transforms import v2 as transforms_v2
 
-from fundamentals.models.image_classifier.cats_and_dogs.dataloaders import MEAN_NORMALIZATION_VALUES, \
-    STD_NORMALIZATION_VALUES
+from fundamentals.models.image_classifier.cats_and_dogs.dataloaders import (
+    MEAN_NORMALIZATION_VALUES,
+    STD_NORMALIZATION_VALUES,
+)
 
 
 def process_image(new_img_path: str, size: tuple[int, int]):
     image = Image.open(fp=new_img_path).convert("RGB")
 
-    transform = transforms_v2.Compose([
-        transforms_v2.ToImage(),
-        transforms_v2.Resize(size=size, antialias=True),
-        transforms_v2.ToDtype(torch.float32, scale=True),
-        transforms_v2.Normalize(
-            mean=MEAN_NORMALIZATION_VALUES, std=STD_NORMALIZATION_VALUES
-        ),
-    ])
+    transform = transforms_v2.Compose(
+        [
+            transforms_v2.ToImage(),
+            transforms_v2.Resize(size=size, antialias=True),
+            transforms_v2.ToDtype(torch.float32, scale=True),
+            transforms_v2.Normalize(
+                mean=MEAN_NORMALIZATION_VALUES, std=STD_NORMALIZATION_VALUES
+            ),
+        ]
+    )
     image_transformed = transform(image).unsqueeze(0)
 
     return image_transformed
-
-
 
 
 def predict_image(model: nn.Module, X_new: torch.Tensor):
